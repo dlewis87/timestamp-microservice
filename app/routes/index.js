0b036ec11c87363bf0c56ec0a666a40f2c792fb6
Danny Lewis <dan_e12@hotmai.com>
2016-05-02 09:58:54 +0000

    Finished app

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,39 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var moment = require("moment");
 
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
+function MakeDate(date){
+	var unix, natural;
+	
+	if(moment.unix(Number(date)).format("MMMM D, YYYY") !== "Invalid date"){
+		unix = date;
+		natural = moment.unix(Number(date)).format("MMMM D, YYYY");
 	}
+	else if(moment(date, "MMMM D, YYYY").format("MMMM D, YYYY") !== "Invalid date"){
+		unix = moment(date, "MMMM D, YYYY").unix();	
+		natural =  date;
+	}
+	else{
+		unix = null;
+		natural = null;
+	}
+	return { "unix": unix, "natural": natural }
+	
+}
 
-	var clickHandler = new ClickHandler();
 
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+module.exports = function (app) {
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+	app.get('/',function (req, res) {
+		res.sendFile(path + '/public/index.html');
+	});
+		
+	app.get('/:date',function (req, res) {
+		var date = req.params.date;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(MakeDate(date));
+	});	
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
